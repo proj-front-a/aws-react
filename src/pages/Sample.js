006839e56bffe2e41def6b2bfa15559fa93435e5
井原 樹/Miki Ihara <mi-ihara@tg-inet.co.jp>
2022-10-25 20:23:37 +0900

    仮想DOM体験のsource追加

--- src/pages/Sample.js
+++ src/pages/Sample.js
@@ -5,6 +5,7 @@ import { RouterSample } from '../component/sample/RouterSample';
 import { SessionSample } from '../component/sample/SessionSample';
 import { UseEffectSample } from '../component/sample/UseEffectSample';
 import { UseStateSample } from '../component/sample/UseStateSample';
+import { VDomSample } from '../component/sample/VDomSample';
 
 export const Sample = () => {
   return (
@@ -12,6 +13,7 @@ export const Sample = () => {
         <h1>Sample Page</h1>
         <h2>1.useState</h2>
         <UseStateSample />
+        <VDomSample />
         <h2>2.react-router-dom</h2>
         <RouterSample />
         <h2>3.react-hook-dom</h2>

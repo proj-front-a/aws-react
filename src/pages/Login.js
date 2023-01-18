53d493c4fb80dec3e2f011e35816b9f1b4694c28
ando-front <114665066+ando-front@users.noreply.github.com>
2023-01-18 23:58:50 +0900

    Login.jsのuseStateを削除して2回目ログイン問題解消

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,33 +16,26 @@ const Login = () => {
     register,
     formState: { errors },
   } = useForm();
-  const [UserProfiles, setUserProfiles] = useState();
   const onSubmit = async (values) => {
     try {
       const res = await axios.get("http://localhost:3004/users");
-      const data = await res.data.map((users) => ({
+      const UserProfiles = await res.data.map((users) => ({
         id: users.id,
         email: users.email,
         password: users.password,
       }));
-      setUserProfiles(data);
       console.log(UserProfiles);
+      const existUser = UserProfiles.find(
+        (users) => users.email === values.email
+      );
+      if (existUser && existUser.password === values.password) {
+        actions.updateUser({ email: values.email });
+        navigate("/");
+      } else {
+        setMsg("Eメール・パスワードの組み合わせが違います");
+      }
     } catch (err) {
       console.log(err);
-    }
-
-    const existUser = UserProfiles.find(
-      // ToDo:「ログイン」を一回押しただけだと反映されない。2回目に反映される（処理の順番に問題あり？）
-      //　解決策の参考https://qiita.com/zoukun97/items/0fb14524c33894cc38dd
-      //             https://qiita.com/soarflat/items/1a9613e023200bbebcb3
-      //             https://qiita.com/shisama/items/61cdcc09dc69fd8d3127
-      (users) => users.email === values.email
-    );
-    if (existUser && existUser.password === values.password) {
-      actions.updateUser({ email: values.email });
-      navigate("/");
-    } else {
-      setMsg("Eメール・パスワードの組み合わせが違います");
     }
   };
 
